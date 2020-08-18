75b5d42ab08b5fe596175f7c901a21ab25066253
nightcrawler <singhajay9009@gmail.com>
2020-08-18 23:37:17 +0530

    Adding additional ways to solve program for SumOfNumbersInString

--- src/main/java/practice/leetcode/mySolution/SumOfNumbersInString.java
+++ src/main/java/practice/leetcode/mySolution/SumOfNumbersInString.java
@@ -4,7 +4,11 @@ public class SumOfNumbersInString {
 
     public static void main(String[] args) {
 
-        System.out.println(new SumOfNumbersInString().sumNumbers("12dsfs54wer3ty"));
+    //    System.out.println(new SumOfNumbersInString().sumNumbers("12dsfs54wer3ty"));
+
+     //   System.out.println(new SumOfNumbersInString().sumNumbersSolution2("12dss54w3"));
+
+        System.out.println(new SumOfNumbersInString().sumNumbersSolution3("23c4d5e3"));
 
     }
 
@@ -35,6 +39,52 @@ public class SumOfNumbersInString {
 
         }
 
+        return sum;
+    }
+
+    //
+
+    public int sumNumbersSolution2(String str) {
+
+        int i=0, n=str.length(), flag=1, startIndex=0, sum=0;
+        String str1 = "0";
+
+        while(i<n) {
+            if (Character.isDigit(str.charAt(i))) {
+                flag = 0;
+                str1 = str.substring(startIndex, i+1);
+            }
+            else{
+                startIndex = i+1;
+            }
+            if(flag == 0 && (!Character.isDigit(str.charAt(i)) || i==n-1)){
+                sum = sum + Integer.parseInt(str1);
+                flag =1;
+            }
+            i++;
+        }
+        return sum;
+    }
+
+
+    // Seems this is the cleanest and shortest solution
+    public int sumNumbersSolution3(String str) {
+
+        int i=0, n=str.length(), flag=1, sum=0;
+        String str1 = "";
+
+        while(i<n) {
+            if (Character.isDigit(str.charAt(i))) {
+                flag = 0;
+                str1 = str1 + str.charAt(i);
+            }
+            if(flag == 0 && (!Character.isDigit(str.charAt(i)) || i == n-1)){
+                sum = sum + Integer.parseInt(str1);
+                str1 = "";
+                flag = 1;
+            }
+            i++;
+        }
         return sum;
     }
 }
