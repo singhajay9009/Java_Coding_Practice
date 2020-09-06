3b0dce705268e472bd8e5e34fd0f6268961642d0
nightcrawler <singhajay9009@gmail.com>
2020-09-06 13:48:55 +0530

    Adding program MoveZerosToEnd

---
+++ src/main/java/practice/leetcode/mySolution/MoveZerosToEnd.java
@@ -0,0 +1,22 @@
+package practice.leetcode.mySolution;
+
+public class MoveZerosToEnd {
+
+    public static void main(String[] args) {
+       //  int [] arr = new int[]{0,1,3,0,2};
+         int [] arr = new int[] {1,1,1,1};
+         int i=0, j = 0;
+         while(j<arr.length){
+             if(arr[j] != 0){
+               int temp = arr[j];
+               arr[j] = arr[i];
+               arr[i] = temp;
+               i++;
+             }
+             j++;
+         }
+        for(int k: arr){
+            System.out.println(k);
+        }
+    }
+}
