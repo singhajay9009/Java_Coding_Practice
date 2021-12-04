3b3d5f8eb7cadacd725fb9b3ee39b46e79b49344
singhajay9009 <singhajay9009@gmail.com>
2021-12-04 11:52:21 +0530

    Adding more programs-1

---
+++ src/main/java/practice/leetcode/mySolution/ClosestCoordinatetoOrigin.java
@@ -0,0 +1,12 @@
+//package practice.leetcode.mySolution;
+//
+//public class ClosestCoordinatetoOrigin {
+//
+//    public static void main(String[] args) {
+//        int [][] arrCoordinates = new int[3][2];
+//        arrCoordinates[0][1]= 1;
+//
+//    }
+//
+//    public int[] getClosestCoordinates()
+//}
